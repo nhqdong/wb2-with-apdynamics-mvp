dedf2a8c5c852daa7f4fd0e69ed02ba561c010a2
Dong Nguyen <nhqdong@gmail.com>
2020-01-24 13:43:21 +0800

    defer appdynamics lib to load after angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {DynamicScriptLoaderService} from './script-loader.service';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,24 @@ import {Component, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wb2-with-apdynamics-mvp';
   isEnabled = true;
 
+  constructor(private dynamicScriptLoader: DynamicScriptLoaderService) {
+  }
+
+  ngOnInit() {
+    this.loadScripts();
+  }
+
   toggle() {
     this.isEnabled = !this.isEnabled;
+  }
+
+  private loadScripts() {
+    this.dynamicScriptLoader.load('appdynamics').then(data => {
+      console.log('appdynamics script loaded');
+    }).catch(error => console.log(error));
   }
 }
